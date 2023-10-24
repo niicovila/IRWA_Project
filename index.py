3134301fe9c478b18bd49a2e7397a158bef181d8
niicovila <nicolas.vila01@estudiant.upf.edu>
2023-10-24 20:16:10 +0200

    reformat

---
+++ index.py
@@ -0,0 +1,24 @@
+import time
+import json
+from collections import defaultdict
+from array import array
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import math
+import numpy as np
+import collections
+from numpy import linalg as la
+import string
+import re
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import nltk
+nltk.download('stopwords')
+from utils import build_terms, read_tweets
+
+def create_index():
+    return 0
+
+if "name" == "__main__":
+    file_path = ''
+    create_index()
